325ad84445abb203c30f1b41b750c45c65d6b064
YuKai Bao <31197028+bao48@users.noreply.github.com>
2018-12-08 14:34:28 -0500

    commit

---
+++ main_v1.java
@@ -0,0 +1,17 @@
+package com.example.v3;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+import java.nio.file.Paths;
+
+public class main_v1 {
+
+    public static String current_path_str = Paths.get(".").toAbsolutePath().normalize().toString();
+
+    public static void main(String[] args) {
+        GUI_v3 gui = new GUI_v3();
+        System.out.println("hello!");
+        gui.setInitialFilePath(current_path_str);
+        DefaultMutableTreeNode head = new DefaultMutableTreeNode(gui.getCurrentDirectory(current_path_str));
+        gui.constructTree(current_path_str, head);
+    }
+}
